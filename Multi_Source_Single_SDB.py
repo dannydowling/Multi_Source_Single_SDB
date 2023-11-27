a3489a0d5a10c3b27e2a9ce57efc8094d5734a35
Danny Dowling <danny.dowling@gmail.com>
2023-11-26 16:43:57 -0900

    Add project files.

---
+++ Multi_Source_Single_SDB.py
@@ -0,0 +1,74 @@
+import os
+import shutil
+import sqlite3
+import time
+import commands
+import urllib
+import shutil
+
+# Function to connect to SMB source
+def connect_to_smb():
+    # Implement SMB connection logic
+    if not os.path.ismount("/mnt"):
+        print("not yet, mounting...")
+        os.system("mount /mnt/smb_share")
+    else:
+        print("mounted")
+    pass
+
+# Function to connect to local disk
+def connect_to_local_disk():
+    # Implement local disk connection logic
+    mount = commands.getoutput('mount -v')
+    mntlines = mount.split('\n')
+    mntpoints = map(lambda line: line.split()[2], mntlines)
+    for x in mntpoints:
+        if not os.path.ismount("/media/" + x):
+            os.system("mount /media/" + x)
+            check_connection_speed("/media/" + x)
+        pass
+    pass
+
+# Function to check connection speed
+def check_connection_speed(connection_function):
+    start_time = time.time()
+    connection_function()
+    end_time = time.time()
+    return end_time - start_time
+
+# Function to copy files and update the SDB database
+def copy_files_and_update_database(source_path, destination_path, database_path):
+    # Implement file copying logic
+    shutil.copy(source_path, destination_path)    
+
+    # Update the SDB database with the link to the copied file
+    conn = sqlite3.connect(database_path)
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO files (path) VALUES (?)", (destination_path,))
+    conn.commit()
+    conn.close()
+
+# Main function
+def main():
+    smb_speed = check_connection_speed(connect_to_smb)
+    local_disk_speed = check_connection_speed(connect_to_local_disk)
+
+    if smb_speed < local_disk_speed:
+        source_connection = connect_to_smb
+        source_path = "/mnt/smb_share"
+    else:
+        source_connection = connect_to_local_disk        
+
+    try:
+        copy_files_and_update_database(source_connection(), /user/kodi/..., "external_database.sdb")
+        print("File copied successfully.")
+    except Exception as e:
+        print(f"Error: {e}")
+        # Fallback to a slower connection in case of failure
+        slower_connection = connect_to_local_disk if source_connection == connect_to_smb else connect_to_local_disk
+        print("Falling back to a slower connection.")
+        copy_files_and_update_database(slower_connection(), "destination/file.txt", "external_database.sdb")
+        print("File copied successfully using the slower connection.")
+
+if __name__ == "__main__":
+    main()
